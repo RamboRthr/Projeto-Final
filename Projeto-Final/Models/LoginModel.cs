5b0818d98b20b32c680dd2386c8d908728c5fb44
Luscas-PandC <lucasfinho32@gmail.com>
2021-10-04 21:51:19 -0300

    token em andamento

---
+++ Projeto-Final/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Final.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage ="Username Is Missing")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is Missing")]
+        public string Password { get; set; }
+    }
+}
---
+++ Projeto-Final/Models/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Final.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage ="NameUser is Missing")]
+        public string NameUser { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is Missing")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is Missing")]
+        public string Password { get; set; }
+    }
+}
