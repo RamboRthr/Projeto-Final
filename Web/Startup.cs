040ba70430f4fae12f397675e0f52cd2beab4835
RamboRthr <arthurbrachtrambo@gmail.com>
2021-10-17 18:49:10 -0300

    teste de pipeline

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -42,6 +42,7 @@ namespace Web
                                       .AllowAnyHeader()
                                       .AllowAnyMethod();
                                   });
+
             });
             services.AddControllers();
 
